17999a4fd063544929d5b11273bbb4c90c89cfbd
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-01-10 14:36:43 +0530

    splitted

---
+++ Goldman Sachs/Split the array into consecutive subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        
+        unordered_map<int,int> cnt, tails;
+        for(int &i : nums) cnt[i]++;
+        for(int &i : nums){
+         if(cnt[i]){
+            cnt[i]--;
+            if(tails[i-1] > 0){
+                tails[i-1]--;
+                tails[i]++;
+            }
+            //complete the set of 3
+            else if(cnt[i+1] && cnt[i+2]){
+                cnt[i+1]--;
+                cnt[i+2]--;
+                tails[i+2]++;
+            }
+            else return false;
+            }
+        }
+        return true;
+        
+    }
+};
